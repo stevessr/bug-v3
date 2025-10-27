8994afa2d4e0452e4374e602693c0b044025feb5
stevessr <steve-ssr@outlook.com>
2025-10-27 10:53:54 +0800

    rm

--- scripts/replace-console-with-logger.mjs
+++
@@ -1,2 +0,0 @@
-// Deprecated: this script has been replaced by scripts/replace-logger-with-console.mjs
-console.log('scripts/replace-console-with-logger.mjs is deprecated. Use scripts/replace-logger-with-console.mjs')
