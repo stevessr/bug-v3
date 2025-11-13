35fba4454031e7b0fb57d2991e8dc6247b3d8e2d
stevessr <steve-ssr@outlook.com>
2025-11-13 22:32:57 +0800

    fix: Import createWebHashHistory in router index

--- src/options/router/index.ts
+++ src/options/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
 // 页面组件
